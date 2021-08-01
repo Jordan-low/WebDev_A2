ca064be1e6397a9b30bebf82eb45d54ae3d08233
Jordan <jawdanlow@gmail.com>
2021-08-01 17:30:43 +0800

    updated stuff, change new page

---
+++ html/html/javascript/dataPage1.js
@@ -0,0 +1,17 @@
+var dataList = [
+    {
+        name:"ABOUT ME",
+        desc:"I am currently a Year 2 Student in Nanyang Polytechnic studying Game Development & Technology.",
+        imgName: "jordanEars.png"
+    },
+    {
+        name:"PORTFOLIO",
+        desc:"Over the years, I've worked on a couple of projects that I'm extremely proud of.",
+        imgName: "jordanEarsPurple.png"
+    },
+    {
+        name:"CONTACT ME",
+        desc:"Be it whether you want to collaborate or for business enquires, feel free to contact me.",
+        imgName: "jordanBunny.png"
+    }
+];
---
+++ html/html/javascript/loadPage1.js
@@ -0,0 +1,70 @@
+//text template
+function addTextContent(title, desc) {
+    let textContainer = document.createElement('div');
+    textContainer.className = "contentText";
+
+    // create h1 title
+    let t = document.createElement("h1");
+    t.appendChild( document.createTextNode(title) );
+    
+    textContainer.appendChild( t );
+    
+    // create p description
+    let p = document.createElement("p");
+    p.appendChild( document.createTextNode(desc) );
+    
+    textContainer.appendChild( p );
+
+    return textContainer;
+}
+
+//photo template
+function addImageContent(title, file) {
+    let imageContainer = document.createElement('div');
+    imageContainer.className = "contentImage";
+    
+    // create img
+    let img = document.createElement('img');
+    img.alt = title;
+    img.src = "images/"+file;
+    
+    imageContainer.appendChild(img);
+    
+    return imageContainer;
+}
+
+function loadContent () {
+    //load page's content
+    
+    let container = document.querySelector(".main-container");
+
+    //loop through all datalist items
+    for (let i = 0; i < dataList.length; i++) {
+        let item = dataList[i];
+
+        let contentContainer = document.createElement('div');
+        contentContainer.className = "contentContainer";
+
+        //switch between text content on the left, image content on the right and vice versa
+        if (i % 2 == 0)
+        {
+        let newTextContent = addTextContent(item.name, item.desc);
+        contentContainer.appendChild(newTextContent);
+
+        let newImageContent = addImageContent(item.name, item.imgName);
+        contentContainer.appendChild(newImageContent);
+        }
+        else
+        {
+        let newImageContent = addImageContent(item.name, item.imgName);
+        contentContainer.appendChild(newImageContent);
+
+        let newTextContent = addTextContent(item.name, item.desc);
+        contentContainer.appendChild(newTextContent);
+        }
+
+        container.appendChild(contentContainer);
+    }
+}
+  
+document.addEventListener('DOMContentLoaded', loadContent);
